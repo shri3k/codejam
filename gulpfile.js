0e0a0fbf2e98d64bc682badf739329e4d3883a3e
Yojan Shrestha <sickgrin@gmail.com>
2014-07-22 01:21:11 -0500

    Modified gulpfile.js to include stylish reporter and deleted bunch of express package that wasn't necessary

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require("gulp");
 var jshint = require("gulp-jshint");
+var stylish = require("jshint-stylish");
 
 gulp.task("lint-js", function(){
 	gulp.src(['!/node_modules', '!/node_modules/**', '/**'])
 		.pipe(jshint('.jshintrc'))
-		.pipe(jshint.reporter('jshint-stylish'));
+		.pipe(jshint.reporter(stylish));
 });
 
 gulp.task('default', ['lint-js'], function(){}); 	
