a158f9ea722ac7748f1a490d7a615e9d7d6a5c64
Yojan Shrestha <sickgrin@gmail.com>
2014-07-23 01:29:30 -0500

    Added mocha test suite

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var jshint = require("gulp-jshint");
 var stylish = require("jshint-stylish");
 
 gulp.task("lint-js", function(){
-	gulp.src(['!/node_modules', '!/node_modules/**', '/**'])
+	gulp.src(['!./node_modules', '!./node_modules/**', './**.js'])
 		.pipe(jshint('.jshintrc'))
 		.pipe(jshint.reporter(stylish));
 });
