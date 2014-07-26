f1e73ec85d2c1c7752c8f4f392b711622eb532e1
Yojan Shrestha <sickgrin@gmail.com>
2014-07-26 15:33:46 -0500

    Added handlebars and fixed the view engine

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'html');
+app.engine('html', require('hbs').__express);
 
 app.use(favicon());
 app.use(logger('dev'));
