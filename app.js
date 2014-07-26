e46f84b24ddd75387814d3e78ce2e32a67d941f2
Yojan Shrestha <sickgrin@gmail.com>
2014-07-26 14:56:18 -0500

    Added hbs(handlebars) and fixed the view engine

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.engine('html', require('hbs').__express);
+app.set('view engine', 'html');
 
 app.use(favicon());
 app.use(logger('dev'));
