afad462507eab30b6d0f40805c525633e7dbba97
Yojan Shrestha <sickgrin@gmail.com>
2014-08-09 22:42:48 -0500

    For fixing the build.

--- test/server/routes/index.js
+++ test/server/routes/index.js
@@ -1,4 +1,4 @@
-var index = require('/routes/index');
+// var index = require('/routes/index');
 describe("Test for a test", function(){
 	it('should equal lol to lol on serverside', function(){
 		"lol".should.equal('lol');
