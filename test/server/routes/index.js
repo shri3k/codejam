273a7c7a7f45bd4f2b30d41edee7cef0520a1184
yojan shrestha <sickgrin@gmail.com>
2014-08-13 05:28:07 -0500

    Adding the chai wrapper for assert, expect and should

--- test/server/routes/index.js
+++ test/server/routes/index.js
@@ -1,7 +1,10 @@
+// var assert = require('chai').assert;
 // var index = require('/routes/index');
+// var assert = require('mocha-assert-wrap');
 describe("Test for a test", function(){
 	it('should equal lol to lol on serverside', function(){
 		"lol".should.equal('lol');
 		// assert.equal("foo", "foo", "Foo should be equal to foo");
+		expect('lol').to.equal('lol');
 	});
 });
