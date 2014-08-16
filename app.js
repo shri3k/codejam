30d4c9afbef100937aae56dda6de5637e8627826
Martin <martin.hinds@gmail.com>
2014-08-16 13:11:35 -0500

    Add mongoose connection err handling.

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 var express      = require('express');
-var mongoose     = require('mongoose');
 var path         = require('path');
 var favicon      = require('static-favicon');
 var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
-var fs           = require('fs');
 
 // added config dir to NODE_PATH in start script - use npm start
 var config       = require('config');
@@ -16,6 +14,7 @@ var quiz         = require('quiz');
 var app = express();
 
 var housekeeper = require('./housekeeper');
+var database    = require('./database');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -37,25 +36,6 @@ app.use('/quiz', quiz);
 
 housekeeper(app);
 
-// Connect to mongodb
-var connect = function () {
-	var options = { server: { socketOptions: { keepAlive: 1 } } };
-	console.log(' connecting to db at: ' + config.db);
-	mongoose.connect(config.db, options);
-};
-connect();
-
-mongoose.connection.on('error', console.log);
-mongoose.connection.on('disconnected', connect);
-
-// Bootstrap models
-fs.readdirSync(__dirname + '/models').forEach(function (file) {
-	if (~file.indexOf('.js')) require(__dirname + '/models/' + file);
-});
-
-// var port = process.env.PORT || 8080;
-
-// app.listen(port);
-// console.log('Listening on port ' + port);
+database(app);
 
 module.exports = app;
