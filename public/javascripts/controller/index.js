1fc3df4fac100b71893c00060f9d2467fe61dbdb
yojan shrestha <sickgrin@gmail.com>
2014-08-23 21:12:19 -0500

    Removing the renaming of the file names

--- public/javascripts/controller/index.js
+++
@@ -1,3 +0,0 @@
-'use strict'
-var app = require('angular').module('quizMe');
-app.controller('LoginController', require('./login'));
--- public/javascripts/service/index.js
+++
@@ -1,2 +0,0 @@
-'use strict';
-var app = require('angular').module('quizMe');
